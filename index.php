cc8afa951a951f653dab03126ac8be5bbf792d88
LeoSallu <L.sallustio99@gmail.com>
2023-04-14 14:38:57 +0200

    funzione _construct

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
 class Movie {
+    public $nome = '';
     public $genere = '';
     public $durata = 90;
     public $voto = 0;
+
+
+function __construct(string $_nome, array $_genere)
+{
+    $this->nome = $_nome;
+    $this->genere = $_genere;
+}
 }
 
-$starWars = new Movie();
-$starWars ->genere='Azione';
+$starWars = new Movie('Star Wars', ['azione','fantascienza','fantasia']);
 var_dump($starWars);
