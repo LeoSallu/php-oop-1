bf7be5a521af8a130aa1ecb463daadd7e2eac4e8
LeoSallu <L.sallustio99@gmail.com>
2023-04-14 14:54:47 +0200

    due oggetti movie inseriti correttamente

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 
 class Movie {
     public $nome = '';
-    public $genere = '';
+    public $genere;
     public $durata = 90;
     public $voto = 0;
 
-
-function __construct(string $_nome, array $_genere)
+public function setDurata($durata){
+    return $durata;
+}
+function __construct(string $_nome, array $_genere, int $_durata, int $_voto)
 {
     $this->nome = $_nome;
     $this->genere = $_genere;
+    $this->setDurata($_durata);
+    $this->voto = $_voto;
 }
 }
 
-$starWars = new Movie('Star Wars', ['azione','fantascienza','fantasia']);
+$starWars = new Movie('Star Wars', ['azione','fantascienza','fantasia'], 90 , 5);
+$lordOfTheRings = new Movie('Il signore degli anelli', ['azione','fantasia'], 120 , 10);
 var_dump($starWars);
+var_dump($lordOfTheRings);
